cd993587046ef45c12c4dc253a70a5efc4c131f8
rtmalone <rtylermalone@gmail.com>
2014-04-17 10:46:20 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Cal
+===
+
+A Unix style command line calendar built with Ruby
---
+++ test/guided_tests.rb
@@ -0,0 +1,6 @@
+$LOAD_PATH << "test"
+$LOAD_PATH << "lib"
+
+require 'test_month'
+require 'test_month_integration'
+require 'test_zellers_congruence'
